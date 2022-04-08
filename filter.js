ca0ac8645076970c9b3a1d3376e962beba2356ce
tshifhiwa <matombot@gmail.com>
2022-04-08 10:59:10 +0200

    landingpage added

--- filter.js
+++ filter.js
@@ -7,37 +7,47 @@ const hbsElem = document.querySelector(".hbsTemplate").innerHTML;
 const carElem = Handlebars.compile(hbsElem);
 
 filterBtn.addEventListener("click", function () {
-let colors =document.getElementById("color").value;
-let brands = document.getElementById("brand").value;
- if (colors && brands) {
+    let colors = document.getElementById("color").value;
+    let brands = document.getElementById("brand").value;
+    if (colors && brands) {
 
-    axios
-        .get(`https://api-tutor.herokuapp.com/v1/cars/make/${brands}/color/${colors}`)
+        axios
+            .get(`https://api-tutor.herokuapp.com/v1/cars/make/${brands}/color/${colors}`)
+            .then(function (result) {
+                cars.innerHTML = carElem({
+                    car: result.data
+                });
+
+            })
+    }
+    else if (colors) {
+        axios
+            .get(`https://api-tutor.herokuapp.com/v1/cars/color/${colors}`)
+            .then(function (result) {
+                cars.innerHTML = carElem({
+                    car: result.data
+                });
+
+            })
+    }else  if (brands) {
+        axios
+            .get(`https://api-tutor.herokuapp.com/v1/cars/make/${brands}`)
+            .then(function (result) {
+                cars.innerHTML = carElem({
+                    car: result.data
+                });
+
+            })
+    }
+    else{
+        axios
+        .get(`https://api-tutor.herokuapp.com/v1/cars`)
         .then(function (result) {
             cars.innerHTML =carElem({
                 car: result.data
             });
 
         })
-}
-else if (colors) {
-    axios
-        .get(`https://api-tutor.herokuapp.com/v1/cars/color/${colors}`)
-        .then(function (result) {
-            cars.innerHTML =carElem({
-                car: result.data
-            });
+    }
 
-        })
-} else if (brands) {
-    axios
-        .get(`https://api-tutor.herokuapp.com/v1/cars/make/${brands}`)
-        .then(function (result) {
-            cars.innerHTML =carElem({
-                car: result.data
-            });
-
-        })
-} 
-    
 })
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const carElem = document.querySelector(".cars")
-
-axios
+const cars = document.querySelector(".cars")
+const brands = document.querySelector(".brands")
+const colors = document.querySelector(".colors")
+const hbsElem = document.querySelector(".hbsTemplate").innerHTML;
+const carElem = Handlebars.compile(hbsElem);
+const showElem = document.querySelector(".showTemplate").innerHTML;
+const showsElem = Handlebars.compile(showElem);
+const shawsElem = document.querySelector(".showsTemplate").innerHTML;
+const showssElem = Handlebars.compile(shawsElem);
+/*axios
 .get("https://api-tutor.herokuapp.com/v1/cars")
 .then(function(result){
     console.log(result.data);
@@ -10,9 +17,36 @@ axios
    lists.innerHTML =`<tr>
    <td> ${car.make} <td><td> ${car.color} <td> <td > ${car.model} <td>
     <tr>`
-   carElem.appendChild(lists);
-   carElem.style.color='blue';
+carElem.appendChild(lists);
+  carElem.style.color='blue';
 });  
 });
-
+*/
 //<th> make <th><th> color<th><th> model <th>
+axios
+        .get(`https://api-tutor.herokuapp.com/v1/makes`)
+        .then(function (result)
+        
+         {
+            brands.innerHTML =showsElem({
+                showbrand: result.data
+            });
+console.log(result.data);
+        })
+
+    axios
+        .get(`https://api-tutor.herokuapp.com/v1/colors`)
+        .then(function (result) {
+            colors.innerHTML =showssElem({
+                showcolor: result.data
+            });
+console.log(result.data)
+        });
+    axios
+        .get(`https://api-tutor.herokuapp.com/v1/cars`)
+        .then(function (result) {
+            cars.innerHTML =carElem({
+                car: result.data
+            });
+
+        })
